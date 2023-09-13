8db1ae388102fc5c6af4c473b570303c981b0440
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2023-09-13 21:06:31 +0300

    Add initial migration

---
+++ DentalCore.Data/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using DentalCore.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DentalCore.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Allergy> Allergies { get; set; }
+    public DbSet<City> Cities { get; set; }
+    public DbSet<Patient> Patients { get; set; }
+    public DbSet<Payment> Payments { get; set; }
+    public DbSet<Procedure> Procedures { get; set; }
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<TreatmentItem> TreatmentItems { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Visit> Visits { get; set; }
+
+    public AppDbContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Allergy>(a =>
+            a.HasKey(nameof(Allergy.PatientId), nameof(Allergy.Name)));
+
+        modelBuilder.Entity<TreatmentItem>(t =>
+            t.HasKey(nameof(TreatmentItem.VisitId), nameof(TreatmentItem.ProcedureId)));
+        
+        base.OnModelCreating(modelBuilder);
+    }
+}
--- DentalCore.Wpf/App.xaml.cs
+++ DentalCore.Wpf/App.xaml.cs
@@ -1,17 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using DentalCore.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-namespace DentalCore.Wpf
+namespace DentalCore.Wpf;
+
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    private static readonly IHost AppHost;
+
+    static App()
     {
+        AppHost = Host.CreateDefaultBuilder()
+            .ConfigureAppConfiguration(builder => builder.AddJsonFile("appsettings.json"))
+            .ConfigureServices((builder, services) =>
+            {
+                var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+                services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+
+            })
+            .Build();
     }
 }
