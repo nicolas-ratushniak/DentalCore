7835fb879cb399c9c3dbfb3ae89cfc8940937169
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2023-09-27 19:10:20 +0300

    Add AuthenticationService

---
+++ DentalCore.Wpf/Services/Authentication/AuthenticationService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using DentalCore.Data.Models;
+using DentalCore.Domain.Exceptions;
+using DentalCore.Domain.Services;
+
+namespace DentalCore.Wpf.Services.Authentication;
+
+public class AuthenticationService : IAuthenticationService
+{
+    public event EventHandler<UserChangedEventArgs>? CurrentUserChanged;
+
+    private readonly IUserService _userService;
+
+    public User? CurrentUser { get; set; }
+    public bool IsLoggedIn => CurrentUser is not null;
+
+    public AuthenticationService(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    public AuthenticationResult LogIn(string login, string password)
+    {
+        try
+        {
+            var user = _userService.Get(login);
+
+            if (!_userService.CheckPassword(user.Id, password))
+            {
+                return AuthenticationResult.InvalidPassword;
+            }
+
+            CurrentUser = user;
+            CurrentUserChanged?.Invoke(this, new UserChangedEventArgs(CurrentUser));
+
+            return AuthenticationResult.Success;
+        }
+        catch (EntityNotFoundException)
+        {
+            return AuthenticationResult.UserNotFound;
+        }
+    }
+
+    public void LogOut()
+    {
+        CurrentUser = null;
+        CurrentUserChanged?.Invoke(this, new UserChangedEventArgs(CurrentUser));
+    }
+}
+
+public enum AuthenticationResult
+{
+    Success,
+    UserNotFound,
+    InvalidPassword
+}
---
+++ DentalCore.Wpf/Services/Authentication/IAuthenticationService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using DentalCore.Data.Models;
+
+namespace DentalCore.Wpf.Services.Authentication;
+
+public interface IAuthenticationService
+{
+    public event EventHandler<UserChangedEventArgs> CurrentUserChanged;
+    public User? CurrentUser { get; set; }
+    public bool IsLoggedIn { get; }
+    public AuthenticationResult LogIn(string login, string password);
+    public void LogOut();
+}
