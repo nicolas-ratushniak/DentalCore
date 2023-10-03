8a6f1869e465c2da886054d14fd65e8ce784ceb5
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2023-10-03 08:04:52 +0300

    Just a part of recent fix

--- DentalCore.Wpf/ViewModels/Inners/TreatmentItemListItemViewModel.cs
+++ DentalCore.Wpf/ViewModels/Inners/TreatmentItemListItemViewModel.cs
@@ -2,9 +2,32 @@
 
 public class TreatmentItemListItemViewModel : BaseViewModel
 {
+    private int _quantity;
+    private bool _isSelected;
     public int Id { get; set; }
-    public bool IsSelected { get; set; }
+
+    public bool IsSelected
+    {
+        get => _isSelected;
+        set
+        {
+            if (value == _isSelected) return;
+            _isSelected = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public int Quantity
+    {
+        get => _quantity;
+        set
+        {
+            if (value == _quantity) return;
+            _quantity = value;
+            OnPropertyChanged();
+        }
+    }
+
     public string Name { get; set; }
-    public int Quantity { get; set; }
     public int Price { get; set; }
 }
