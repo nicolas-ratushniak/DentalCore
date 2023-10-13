bcfaa3d368a7581e08841b99acbba7e8274ca822
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2023-10-13 17:17:57 +0300

    Add async relay command

---
+++ DentalCore.Wpf/Commands/AsyncCommand.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DentalCore.Wpf.Commands;
+
+public abstract class AsyncCommand : ICommand
+{
+    public event EventHandler? CanExecuteChanged;
+
+    private readonly Action<Exception>? _onException;
+    private bool _isExecuting;
+
+    public bool IsExecuting
+    {
+        get => _isExecuting;
+        set
+        {
+            _isExecuting = value;
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    protected AsyncCommand(Action<Exception>? onException)
+    {
+        _onException = onException;
+    }
+
+    public virtual bool CanExecute(object? parameter)
+    {
+        return !_isExecuting;
+    }
+
+    public async void Execute(object? parameter)
+    {
+        _isExecuting = true;
+
+        try
+        {
+            await ExecuteAsync(parameter);
+        }
+        catch (Exception ex)
+        {
+            _onException?.Invoke(ex);
+        }
+
+        _isExecuting = false;
+    }
+
+    protected abstract Task ExecuteAsync(object? parameter);
+}
---
+++ DentalCore.Wpf/Commands/AsyncRelayCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DentalCore.Wpf.Commands;
+
+public class AsyncRelayCommand : AsyncCommand
+{
+    private readonly Func<Task> _execute;
+    private readonly Func<bool> _canExecute;
+
+    public AsyncRelayCommand(Func<Task> execute, Func<bool> canExecute, Action<Exception>? onException = null) :
+        base(onException)
+    {
+        _execute = execute;
+        _canExecute = canExecute;
+    }
+
+    public override bool CanExecute(object? parameter)
+    {
+        return base.CanExecute(parameter) && _canExecute();
+    }
+
+    protected override async Task ExecuteAsync(object? parameter)
+    {
+        await _execute();
+    }
+}
