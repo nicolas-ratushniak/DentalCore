5b6778052d2604e8489e42512d900645837bbb8e
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2023-12-31 17:01:53 +0200

    Edit CityCreateDto
    Took 14 minutes

--- DentalCore.Domain/Dto/CityCreateDto.cs
+++ DentalCore.Domain/Dto/CityCreateDto.cs
@@ -4,7 +4,7 @@ namespace DentalCore.Domain.Dto;
 
 public class CityCreateDto
 {
-    [Required(ErrorMessage = "Поле Назва є обов'язковим")]
-    [StringLength(30, MinimumLength = 2, ErrorMessage = "Назва повинна мати від 2 до 30 символів")]
+    [Required(ErrorMessage = "Назва міста обов'язкова")]
+    [StringLength(30, MinimumLength = 2, ErrorMessage = "Назва міста повинна мати від 2 до 30 символів")]
     public string Name { get; set; }
 }
